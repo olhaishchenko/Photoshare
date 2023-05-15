d52e8451ba9da0d8f65779d0d967f28575a5ec59
User <user@Users-MacBook-Pro.local>
2023-05-15 12:51:20 +0300

    update repository/comments and schemas.py - add code for comments

---
+++ src/repository/comments.py
@@ -0,0 +1,114 @@
+from typing import List
+
+from sqlalchemy.orm import Session
+from sqlalchemy import and_, func
+
+from src.database.models import User, Comment, Role
+from src.schemas import CommentBase
+
+
+async def add_comment(image_id: int, body: CommentBase, db: Session, user: User) -> Comment:
+    """
+    Adds a new comment to the database.
+    Args:
+        image_id (int): The id of the image to which this comment belongs.
+        body (CommentBase): A CommentBase object containing information about the new comment.
+
+    :param image_id: int: Identify the image that the comment is being added to
+    :param body: CommentBase: Specify the type of data that is expected to be passed in
+    :param db: Session: Access the database
+    :param user: User: Get the user_id from the logged-in user
+    :return: A comment object
+    """
+    new_comment = Comment(comment=body.comment, image_id=image_id, user_id=user.id)
+    db.add(new_comment)
+    db.commit()
+    db.refresh(new_comment)
+    return new_comment
+
+
+async def edit_comment(comment_id: int, body: CommentBase, db: Session, user: User) -> Comment | None:
+    """
+    Allows a user to edit their own comment.
+    Args:
+        comment_id (int): The id of the comment that is being edited.
+        body (CommentBase): The new text for the comment.
+
+    :param comment_id: int: Find the comment in the database
+    :param body: CommentBase: Pass the data from the request body to this function
+    :param db: Session: Connect to the database
+    :param user: User: Check if the user is an admin, moderator or the author of the comment
+    :return: A comment object
+    """
+    comment = db.query(Comment).filter(Comment.id == comment_id).first()
+    if comment:
+        if user.roles in [Role.admin, Role.moderator] or comment.user_id == user.id:
+            comment.comment = body.comment
+            comment.updated_at = func.now()
+            db.commit()
+    return comment
+
+
+async def delete_comment(comment_id: int, db: Session, user: User) -> None:
+    """
+    Deletes a comment from the database. The comment can be deleted by Admin and Moderator
+    Args:
+        comment_id (int): The id of the comment to be deleted.
+        db (Session): A connection to the database.
+        user (User): The user who is deleting this comment.
+
+    :param comment_id: int: Identify the comment to be deleted
+    :param db: Session: Connect to the database
+    :param user: User: Check if the user is Admin or Moderator and authorized to delete a comment
+    :return: The comment that was deleted
+    """
+    comment = db.query(Comment).filter(Comment.id == comment_id).first()
+    if comment:
+        if user.roles in [Role.admin, Role.moderator]:
+            db.delete(comment)
+            db.commit()
+    return comment
+
+
+async def get_comment_by_id(comment_id: int, db: Session, user: User) -> Comment | None:
+    """
+    Returns a comment from the database by comment_id.
+    Args:
+        comment_id (int): The id of the comment to be returned.
+        db (Session): A connection to the database.
+        user (User): The currently logged-in user.
+
+    :param comment_id: int: Specify the id of the comment that we want to retrieve
+    :param db: Session: Access the database
+    :param user: User: Check if the user is authorized to see comment
+    :return: The comment with the given id, if it exists
+    """
+    return db.query(Comment).filter(and_(Comment.id == comment_id, Comment.user_id == user.id)).first()
+
+
+async def get_comments_by_user_id(user_id: int, db: Session) -> List[Comment] | None:
+    """
+    Returns a list of comments made by the user with the given id.
+    If no such user exists, it returns None.
+
+    :param user_id: int: Specify the user_id of the user whose comments we want to retrieve
+    :param db: Session: Pass the database session to the function
+    :return: A list of comments
+    """
+    return db.query(Comment).filter(Comment.user_id == user_id).all()
+
+
+async def get_user_comments_by_image(user_id: int, image_id: int, db: Session) -> List[Comment] | None:
+    """
+    Returns a list of comments for a given user and image.
+    Args:
+        user_id (int): The id of the user whose comments are being retrieved.
+        image_id (int): The id of the image whose comments are being retrieved.
+
+    :param user_id: int: Filter the comments by user_id
+    :param image_id: int: Filter the comments by image_id
+    :param db: Session: Pass the database session to the function
+    :return: A list of comments, or none if the user doesn't exist
+    """
+    return db.query(Comment).filter(and_(Comment.user_id == user_id, Comment.image_id == image_id)).all()
+
--- src/schemas.py
+++ src/schemas.py
@@ -1,2 +1,24 @@
 from pydantic import BaseModel, Field, EmailStr
 from pydantic.types import date
+
+
+class CommentBase(BaseModel):
+    comment: str = Field(max_length=500)
+
+
+class CommentModel(CommentBase):
+    id: int
+    created_at: datetime
+    updated_at: Optional[datetime]
+    user_id: int
+    image_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class CommentUpdate(CommentModel):
+    updated_at = datetime
+
+    class Config:
+        orm_mode = True
