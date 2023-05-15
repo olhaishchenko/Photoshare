288c85a8ea7393f6e6d46bf362dd15e37e4aee7f
User <user@Users-MacBook-Pro.local>
2023-05-15 15:45:49 +0300

    fix conflicts

--- src/config/detail.py
+++ src/config/detail.py
@@ -1,2 +1,19 @@
+ACCOUNT_AlREADY_EXISTS = "Account already exists"
+INVALID_EMAIL = "Invalid email"
+EMAIL_NOT_CONFIRMED = "Email not confirmed"
+INVALID_PASSWORD = "Invalid password"
+INVALID_REFRESH_TOKEN = "Invalid refresh token"
+VERIFICATION_ERROR = "Verification error"
+NOT_FOUND = 'Not found'
+EMAIL_IS_EXISTS = 'Email is exists'
+ALREADY_CONFIRMED = "Your email is already confirmed"
+EMAIL_CONFIRMED = "Email confirmed"
+CHECK_CONFIRMATION = "Check your email for confirmation."
+INVALID_TOKEN = 'Invalid scope for token'
+NOT_VALIDATE = 'Could not validate credentials'
+INVALID_TOKEN_EMAIL = "Invalid token for email verification"
+SUCCESS_CREATE_USER = "Success create user"
+USER_NOT_ACTIVE = "User is not active"
+
 COMMENT_NOT_FOUND = "Comment not found or not available."
 OPERATION_FORBIDDEN = "Operation forbidden"
--- src/schemas.py
+++ src/schemas.py
@@ -1,6 +1,43 @@
 from pydantic import BaseModel, Field, EmailStr
 from pydantic.types import date
 
+from src.database.models import Role
+
+
+class UserModel(BaseModel):
+    username: str = Field(min_length=5, max_length=12)
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=8)
+
+
+class UserDb(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    avatar: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserResponse(BaseModel):
+    class UserResponse(BaseModel):
+        user: UserDb
+        detail: str = "User successfully created"
+
+    class Config:
+        orm_mode = True
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class RequestEmail(BaseModel):
+    email: EmailStr
+
 
 class CommentBase(BaseModel):
     comment: str = Field(max_length=500)
