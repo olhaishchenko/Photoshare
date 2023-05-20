e2f0d9ae64e68fd3eec4f137ed820061a10a3f20
VOliinyk1 <vov.25.98@gmail.com>
2023-05-20 10:57:03 +0300

    update_tests_users_repo_routes

--- src/config/config.py
+++ src/config/config.py
@@ -2,22 +2,22 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlalchemy_database_url: str = 'postgresql+psycopg2://postgres:1234@localhost:5432/postgres'
-    secret_key: str = 'secret_key'
-    algorithm: str = 'HS256'
-    mail_username: str = 'example@meta.ua'
-    mail_password: str = 'password'
-    mail_from: str = 'example@meta.ua'
-    mail_port: int = 465
-    mail_server: str = 'smtp.meta.ua'
-    redis_host: str = 'localhost'
-    redis_port: int = 6379
-    cloudinary_name: str = 'dvrbkvnto'
-    cloudinary_api_key: int = 661881531523377
-    cloudinary_api_secret: str = 'VFzARWdPhgyAHcBLntfBpemyuHk'
+    sqlalchemy_database_url: str
+    secret_key: str
+    algorithm: str
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_port: int
+    mail_server: str
+    redis_host: str
+    redis_port: int
+    cloudinary_name: str
+    cloudinary_api_key: int
+    cloudinary_api_secret: str
 
     class Config:
-        env_file = ".env"
+        env_file = ".example.env"
         env_file_encoding = "utf-8"
 
 
