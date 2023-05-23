5a857abc9824c0472d1bf06f380bd24abe04c653
VOliinyk1 <vov.25.98@gmail.com>
2023-05-23 19:34:35 +0300

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -3,23 +3,22 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
 
-    sqlalchemy_database_url: str = 'postgresql+psycopg2://postgres:1234@localhost:5432/postgres'
-    secret_key: str = 'secret_key'
-    algorithm: str = 'HS256'
-    mail_username: str = 'volodymyr.oliinyk@meta.ua'
-    mail_password: str = 'ReptilesOwnUs16'
-    mail_from: str = 'volodymyr.oliinyk@meta.ua'
-    mail_port: int = 465
-    mail_server: str = 'smtp.meta.ua'
-    redis_host: str = 'localhost'
-    redis_port: int = 6379
-    cloudinary_name: str = 'dvrbkvnto'
-    cloudinary_api_key: int = 661881531523377
-    cloudinary_api_secret: str = 'VFzARWdPhgyAHcBLntfBpemyuHk'
+    sqlalchemy_database_url: str
+    secret_key: str
+    algorithm: str
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_port: int
+    mail_server: str
+    redis_host: str
+    redis_port: int
+    cloudinary_name: str
+    cloudinary_api_key: int
+    cloudinary_api_secret: str
 
     class Config:
-        case_sensitive = True
-        env_file = "../../.env"
+        env_file = ".env"
         env_file_encoding = "utf-8"
 
 
